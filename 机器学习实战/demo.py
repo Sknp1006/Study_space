0d0e08f43822136de21b28598622fcef2a8d1941
Sknp1006 <41496773+Sknp1006@users.noreply.github.com>
2019-02-26 08:37:16 +0800

    机器学习

---
+++ 机器学习实战/demo.py
@@ -0,0 +1,14 @@
+from numpy import *
+import numpy as np
+# group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+# t = tile([0,0], (4,1)) - group
+# print(group.shape[0])
+# print(t**2)
+
+# a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# c=np.array([[1,2,3],[4,5,6]])
+#
+# print(c.dot(a))
+
+zero = zeros((1, 5))
+print(zero)
