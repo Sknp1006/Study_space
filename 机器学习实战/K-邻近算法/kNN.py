100f454d709c34bc9b798f164b60626d3ffd8b81
Sknp1006 <41496773+Sknp1006@users.noreply.github.com>
2019-02-27 20:41:59 +0800

    kNN

--- 机器学习实战/K-邻近算法/kNN.py
+++ 机器学习实战/K-邻近算法/kNN.py
@@ -1,22 +1,33 @@
 from numpy import *
+import numpy as np
 import operator
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from os import listdir
+
 
 def createDataSet():
-    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-    labels = ['A', 'A', 'B', 'B']
+    group = array([[1, 101], [5, 89], [108, 5], [115, 8]])
+    labels = ['爱情片', '爱情片', '动作片', '动作片']
     return group, labels
 
-def classify0(inX, dataSet, labels, k):
+# 分类器
+def classify0(inX, dataSet, labels,  k):
+    # shape返回行列个数，0表示行，1表示列
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat**2
+    sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
+    distances = sqDistances ** 0.5
+    # argsort()存的是从小到大排序后的索引值
     sortDistIndicies = distances.argsort()
     classCount = {}
+    # 得到前k个点的距离
     for i in range(k):
         voteIlable = labels[sortDistIndicies[i]]
         classCount[voteIlable] = classCount.get(voteIlable, 0) + 1
+    # operator.itemgetter(1) 获取第一个域的值
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
@@ -38,3 +49,211 @@ def file2matrix(filename):
     return retuenMat, classLabelVector
 
 
+def showdatas(datingDataMat, datingLabels):
+    # 设置汉字格式
+    font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
+    # 将fig画布分隔成1行1列,不共享x轴和y轴,fig画布的大小为(13,8)
+    # 当nrow=2,nclos=2时,代表fig画布被分为四个区域,axs[0][0]表示第一行第一个区域
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=False, figsize=(13, 8))
+
+    numberOfLabels = len(datingLabels)
+    LabelsColors = []
+    for i in datingLabels:
+        if i == 1:
+            LabelsColors.append('black')
+        if i == 2:
+            LabelsColors.append('orange')
+        if i == 3:
+            LabelsColors.append('red')
+    # 画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第二列(玩游戏)数据画散点数据,散点大小为15,透明度为0.5
+    axs[0][0].scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 1], color=LabelsColors, s=15, alpha=.5)
+    # 设置标题,x轴label,y轴label
+    axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比', FontProperties=font)
+    axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
+    axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占比', FontProperties=font)
+    plt.setp(axs0_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
+
+    # 画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+    axs[0][1].scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
+    # 设置标题,x轴label,y轴label
+    axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰激淋公升数', FontProperties=font)
+    axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
+    axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰激淋公升数', FontProperties=font)
+    plt.setp(axs1_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
+
+    # 画出散点图,以datingDataMat矩阵的第二(玩游戏)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+    axs[1][0].scatter(x=datingDataMat[:, 1], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
+    # 设置标题,x轴label,y轴label
+    axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰激淋公升数', FontProperties=font)
+    axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比', FontProperties=font)
+    axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰激淋公升数', FontProperties=font)
+    plt.setp(axs2_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
+    # 设置图例
+    didntLike = mlines.Line2D([], [], color='black', marker='.',
+                              markersize=6, label='didntLike')
+    smallDoses = mlines.Line2D([], [], color='orange', marker='.',
+                               markersize=6, label='smallDoses')
+    largeDoses = mlines.Line2D([], [], color='red', marker='.',
+                               markersize=6, label='largeDoses')
+    # 添加图例
+    axs[0][0].legend(handles=[didntLike, smallDoses, largeDoses])
+    axs[0][1].legend(handles=[didntLike, smallDoses, largeDoses])
+    axs[1][0].legend(handles=[didntLike, smallDoses, largeDoses])
+
+    # 显示图片
+    plt.show()
+
+
+"""
+函数说明:分类器测试函数
+Parameters:
+	无
+Returns:
+	normDataSet - 归一化后的特征矩阵
+	ranges - 数据范围
+	minVals - 数据最小值
+Modify:
+	2017-03-24
+"""
+
+
+# 归一化函数
+def autoNorm(dataSet):
+    # 获得数据的最小值
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    # 最大值和最小值的范围
+    ranges = maxVals - minVals
+    # shape(dataSet)返回dataSet的矩阵行列数
+    normDataSet = np.zeros(np.shape(dataSet))
+    # 返回dataSet的行数
+    m = dataSet.shape[0]
+    # 原始值减去最小值
+    normDataSet = dataSet - np.tile(minVals, (m, 1))
+    # 除以最大和最小值的差,得到归一化数据
+    normDataSet = normDataSet / np.tile(ranges, (m, 1))
+    # 返回归一化数据结果,数据范围,最小值
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    # 打开的文件名
+    filename = "datingTestSet2.txt"
+    # 将返回的特征矩阵和分类向量分别存储到datingDataMat和datingLabels中
+    datingDataMat, datingLabels = file2matrix(filename)
+    # 取所有数据的百分之十
+    hoRatio = 0.10
+    # 数据归一化,返回归一化后的矩阵,数据范围,数据最小值
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # 获得normMat的行数
+    m = normMat.shape[0]
+    # 百分之十的测试数据的个数
+    numTestVecs = int(m * hoRatio)
+    # 分类错误计数
+    errorCount = 0.0
+
+    for i in range(numTestVecs):
+        # 前numTestVecs个数据作为测试集,后m-numTestVecs个数据作为训练集
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 4)
+        print("分类结果:%s\t真实类别:%d" % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("错误率:%f%%" % (errorCount / float(numTestVecs) * 100))
+
+
+"""
+函数说明:通过输入一个人的三维特征,进行分类输出
+Parameters:
+	无
+Returns:
+	无
+Modify:
+	2017-03-24
+"""
+
+
+def classifyPerson():
+    # 输出结果
+    resultList = ['讨厌', '有些喜欢', '非常喜欢']
+    # 三维特征用户输入
+    precentTats = float(input("玩视频游戏所耗时间百分比:"))
+    ffMiles = float(input("每年获得的飞行常客里程数:"))
+    iceCream = float(input("每周消费的冰激淋公升数:"))
+    # 打开的文件名
+    filename = "datingTestSet2.txt"
+    # 打开并处理数据
+    datingDataMat, datingLabels = file2matrix(filename)
+    # 训练集归一化
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # 生成NumPy数组,测试集
+    inArr = np.array([ffMiles, precentTats, iceCream])
+    # 测试集归一化
+    norminArr = (inArr - minVals) / ranges
+    # 返回分类结果
+    classifierResult = classify0(norminArr, normMat, datingLabels, 3)
+    # 打印结果
+    print("你可能%s这个人" % (resultList[classifierResult - 1]))
+
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    hwLabels = []  # [0-9]的数字，整形
+    trainingFileList = listdir('.\\digits\\trainingDigits')  # load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        # [i, :] 表示取第i行
+        trainingMat[i, :] = img2vector(r'.\digits\trainingDigits/%s' % fileNameStr)
+    testFileList = listdir(r'.\digits\testDigits')  # iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('.\digits\\testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 4)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
+
+
+if __name__ == '__main__':
+    # 电影分类
+    # group, labels = createDataSet()
+    # test = [10, 100]
+    # test_class = classify0(test, group, labels, 3)
+    # print(test_class)
+
+    # 将三个不同的表可视化
+    # datingDateMat, datingLabels = file2matrix('datingTestSet2.txt')
+    # showdatas(datingDateMat, datingLabels)
+
+    # 测试算法，得到错误率
+    # datingClassTest()
+
+    # 完成品，用来判断你是否喜欢他
+    # classifyPerson()
+
+    handwritingClassTest()
